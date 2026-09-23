7f2d9518ee49174278e50758dc409640dc0c8453
Aidar <atakun.study@gmail.com>
2026-09-23 22:06:04 +0500

    feat: добавлена базовая reply-клавиатура

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -11,9 +11,20 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+main_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="➕ Добавить привычку")],
+        [KeyboardButton(text="📋 Мои привычки"), KeyboardButton(text="❓ Помощь")]
+    ],
+    resize_keyboard=True 
+)
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
-    await message.answer("Привет! Я твой трекер привычек")
+    await message.answer(
+        "Привет! Я твой трекер привычек. Выбери действие ниже 👇",
+        reply_markup=main_kb
+    )
 
 @dp.message(Command("help"))
 async def command_help_handler(message: Message):
